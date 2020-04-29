c0c4ae50a45edccb3980a2830e2c05ea8cced6f3
Milosz Gawel <mgawel@gmail.com>
2020-04-29 20:21:14 +0200

    Updates

---
+++ precache-manifest.04776e30189cb32d770ff47c2a77c011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2be044154caa4c971932ce30dae1b3",
+    "url": "/giphlr/index.html"
+  },
+  {
+    "revision": "ebe73948bc82b083e549",
+    "url": "/giphlr/static/css/main.8673f4bf.chunk.css"
+  },
+  {
+    "revision": "d4495ff4308853bc7105",
+    "url": "/giphlr/static/js/2.b82468df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/giphlr/static/js/2.b82468df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe73948bc82b083e549",
+    "url": "/giphlr/static/js/main.b1089938.chunk.js"
+  },
+  {
+    "revision": "7f32f905cdf0c38cdf16",
+    "url": "/giphlr/static/js/runtime-main.667d6486.js"
+  },
+  {
+    "revision": "1b99dd9695be257f80c87813c7736ac1",
+    "url": "/giphlr/static/media/giphy_main.1b99dd96.jpg"
+  }
+]);
